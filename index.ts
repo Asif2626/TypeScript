4db84aa13c1ea870d6a899c501476407940ccfb5
AsifAliDeveloper <letmeetasif@email.com>
2026-01-28 20:09:36 +0500

    complete

--- index.ts
+++ index.ts
@@ -1,24 +1,3 @@
-let message: string = "Hello TypeScript";
-console.log(message);
-let id: number = 1;
-let username: string = "Asif;";
-let isActive: boolean = true;
-let value: null = null;
-let notDefined: undefined = undefined;
-console.log(
-  "id: ",
-  id,
-  "userName: ",
-  username,
-  "isActive: ",
-  isActive,
-  "value: ",
-  value,
-  "notDefined: ",
-  notDefined,
-);
+let age: number = 10;
 
-const fullName: string = "Asif Ali";
-const age: number = 24;
-const isLoggedIn: boolean = true;
-console.log("name: ", fullName, "age: ", age, "isLoggedIn: ", isLoggedIn);
+console.log(age);
